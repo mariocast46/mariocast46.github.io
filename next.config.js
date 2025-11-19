55fdbc14057d9057d59b424ffa0d39bb3fc16a01
mariocast46 <mariocasstt@gmail.com>
2025-11-19 18:08:41 +0100

    Configurar Next para Vercel

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
+  // puedes dejar esto vacío o con las opciones que ya tuvieras
 };
 
 module.exports = nextConfig;
